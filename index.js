25363e8af9c06eeaf497b8e53123b214b7d2c785
AjiPerdana38 <ajicooljazz38@gmail.com>
2022-12-09 11:03:36 +0700

    setting fix fix fix for last

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const store = new sessionStore({
 // store.sync();
   
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESS_SECRET,
   resave: false,
   saveUninitialized: true,
   proxy: true,
